ba6ddd73e8be4adb32b66fec4a13e06052636991
Maikel <43137751+maikelpoot@users.noreply.github.com>
2023-04-11 16:03:28 +0200

    Check for empty string or time.isZero (#29)
    * Check for empty string or parsed version `0001-01-01`
    
    * update ChangeLog
    
    * Use isZero instead of string compare
    
    * Update changelog
    
    * Less verbose programming :)

--- keyhub/data_source_vaultrecord.go
+++ keyhub/data_source_vaultrecord.go
@@ -337,7 +337,13 @@ func dataSourceVaultRecordRead(ctx context.Context, d *schema.ResourceData, m in
 		})
 	}
 
-	if err := d.Set("enddate", vaultRecord.EndDate.Format("2006-01-02")); err != nil {
+	var tmpEndDate string
+
+	if !vaultRecord.EndDate.IsZero() {
+		tmpEndDate = vaultRecord.EndDate.Format("2006-01-02")
+	}
+
+	if err := d.Set("enddate", tmpEndDate); err != nil {
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
 			Summary:  "Could not set value for enddate",
